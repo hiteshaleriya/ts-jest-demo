dcf1c523215b9533e9763268ef29d69e41714df0
Hitesh Aleriya <hiteshaleriya@gmail.com>
2018-04-07 14:23:44 +0530

    Updated jest config file.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+    // The transform config just tells jest to use ts-jest for ts / tsx files
     transform: {
         "^.+\\.tsx?$": "ts-jest",
     },
+    // The testRegex tells Jest to look for tests in any __tests__ folder AND also 
+    // any files anywhere that use the (.test|.spec).(js|jsx|ts|tsx) extension e.g. abc.spec.ts etc.
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+    // The moduleFileExtensions tells jest to recognize our file extensions.
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
     //mapCoverage: true,
